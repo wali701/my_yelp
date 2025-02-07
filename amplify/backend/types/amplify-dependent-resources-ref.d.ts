a057f768aeb7e59fd4b044ea8b9bb0d5570a6b90
Software Development3 <Software Development3@outsource-kd.ng>
2025-02-07 13:14:54 +0100

    configuring amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "myyelp": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
