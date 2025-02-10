a83ada31b6e0839209e13217a4e0b0cfb86f100d
abubakar_s <salihaabubakar2000@gmail.com>
2025-02-10 15:00:21 +0100

    signout issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'; 
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Auth } from '@aws-amplify/auth'
+
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports'; // Automatically created by Amplify CLI
 
 Amplify.configure(awsconfig);
 
 function App() {
+  const handleSignOut = async () => {
+    try {
+      await Auth.signOut(); // Correct usage of Auth.signOut()
+    } catch (error) {
+      console.error('Error signing out: ', error);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Welcome to My Yelp App</h1>
-      {/* Display the sign-out button when the user is signed in */}
-      <AmplifySignOut />
+      <button onClick={handleSignOut}>Sign Out</button>  {/* Custom sign-out button */}
     </div>
   );
 }
