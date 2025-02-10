6df1a98fca62dfefb14e33bd8b8b8d6b7762fc15
Software Development3 <Software Development3@outsource-kd.ng>
2025-02-10 11:04:11 +0100

    integrating graphql

--- my_yelp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ my_yelp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "myyelp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "myyelpc4dbe04d": {
       "AppClientID": "string",
