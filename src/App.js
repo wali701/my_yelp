50ccda8db4827bc008cd4fc71d395a1549e685ff
Software Development3 <Software Development3@outsource-kd.ng>
2025-02-10 12:46:44 +0100

    another step for the building

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports'; // Automatically created by Amplify CLI
 
 Amplify.configure(awsconfig);
